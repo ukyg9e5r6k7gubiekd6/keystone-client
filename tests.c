ee2206b49eb95b3c6f06032e4a33ca828cd578f8
Mitya_Eremeev <mitossvyaz@mail.ru>
2021-11-12 17:56:30 +0300

    Formed test into functions for readability.

--- tests.c
+++ tests.c
@@ -2,28 +2,37 @@
 
 #include "keystone-client.h"
 
-int main(void)
-{
-	keystone_context_t context;
-	enum keystone_error result;
-
-	result = keystone_global_init();
+enum keystone_error
+init() {
+	enum keystone_error result = keystone_global_init();
 	printf("keystone_global_init result: %d\n", result);
+	return result;
+}
 
+enum keystone_error
+start(keystone_context_t *context){
 	/* we must initialise with false values in order keystone_start could
 	 * assign default values for context */
 	/* TODO: eliminate preliminary assignment - do default initialisation
 	 * inside keystone_start */
-	context.curl_error = NULL;
-	context.json_error = NULL;
-	context.keystone_error = NULL;
-	context.allocator = NULL;
-	result = keystone_start(&context);
+	context->curl_error = NULL;
+	context->json_error = NULL;
+	context->keystone_error = NULL;
+	context->allocator = NULL;
+	enum keystone_error result = keystone_start(context);
 	printf("keystone_start result: %d\n", result);
+	return result;
+}
 
-	result = keystone_set_debug(&context, 1);
+enum keystone_error
+set_debug(keystone_context_t *context){
+	enum keystone_error result = keystone_set_debug(context, 1);
 	printf("keystone_set_debug result: %d\n", result);
+	return result;
+}
 
+enum keystone_error
+authenticate(keystone_context_t *context){
 	char *temp_var = NULL;
 	const char *url;
 	temp_var = getenv("KSTEST_ADMIN_URL");
@@ -57,21 +66,39 @@ int main(void)
 	else {
 		password = "secret";
 	}
-	result = keystone_authenticate(&context, url, tenant_name, username,
-								   password);
+	enum keystone_error result = keystone_authenticate(context, url,
+			tenant_name, username, password);
 	printf("keystone_authenticate result: %d\n", result);
+	return result;
+}
 
-	const char *token = keystone_get_auth_token(&context);
+void print_token (keystone_context_t *context){
+	const char *token = keystone_get_auth_token(context);
 	if (token != NULL) {
 		printf("token: %s\n", token);
 	}
+}
 
-	keystone_end(&context);
+void end (keystone_context_t *context){
+	keystone_end(context);
 	printf("keystone_end called\n");
+}
 
+void cleanup (){
 	keystone_global_cleanup();
 	printf("keystone_global_cleanup called\n");
+}
 
+int main(void)
+{
+	keystone_context_t context;
+	init();
+	start(&context);
+	set_debug(&context);
+	authenticate(&context);
+	print_token(&context);
+	end(&context);
+	cleanup();
 	return 0;
 }
 
